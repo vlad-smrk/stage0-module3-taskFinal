5d0967f7ffab05a3267e385621d2237b06d44f4c
Vladislav Samorukov <smrk.vld@gmail.com>
2023-02-10 20:33:18 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first = number % 10;
+        number /= 10;
+        int second = number % 10;
+        number /= 10;
+        System.out.println(first + "" + second + "" + number);
     }
+
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,5 +2,7 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
+        double temperatureF = temperatureCelsius * 1.8 + 32;
+        System.out.println(temperatureF);
     }
 }
